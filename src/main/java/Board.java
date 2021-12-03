68e986ed655dd489077263432c30f2b1acad29ac
Mimo604 <56412271+SelimAhmed@users.noreply.github.com>
2021-12-03 12:29:57 -0500

    Delete useless classes

---
+++ src/main/java/Board.java
@@ -0,0 +1,9 @@
+import javafx.scene.layout.GridPane;
+
+public class Board {
+   final GridPane gp_CHESS_BOARD;
+
+   public Board(GridPane chessBoard) {
+      gp_CHESS_BOARD = chessBoard;
+   }
+}
