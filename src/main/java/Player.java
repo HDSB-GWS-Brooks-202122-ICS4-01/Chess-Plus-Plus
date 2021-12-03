73a4ca42666fb41fee1a0d2330ae37a1f19d8222
Mimo604 <56412271+SelimAhmed@users.noreply.github.com>
2021-12-03 13:25:59 -0500

    Added classes
    Worked on player class and Board class and Game controller class
    Added methods from UML

---
+++ src/main/java/Player.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Player {
+   private final String NAME;
+
+   final ArrayList<Piece> LIVE_PIECES = new ArrayList<Piece>();
+   final ArrayList<Piece> DEAD_PIECES = new ArrayList()<Piece>();
+
+   Player(String name) {
+      NAME = name;
+   }
+}
