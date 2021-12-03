eb670410739ec981e541e041fcf7af245b3b5162
Mimo604 <56412271+SelimAhmed@users.noreply.github.com>
2021-12-03 12:34:11 -0500

    Update constant names

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -2,40 +2,40 @@ public class Constants {
 
     public static final class pieceIDs{
         
-        public static final int BlackKing = 0;
-        public static final int BlackQueen = 1;
-        public static final int BlackKingsBishop = 2;
-        public static final int BlackQueensBishop = 3;
-        public static final int BlackKingsHorse = 4;
-        public static final int BlackQueensHorse = 5;
-        public static final int BlackKingsRook = 6;
-        public static final int BlackQueensRook = 7;
-        public static final int BlackPawnZero = 8;
-        public static final int BlackPawnOne = 9;
-        public static final int BlackPawnTwo = 10;
-        public static final int BlackPawnThree = 11;
-        public static final int BlackPawnFour = 12;
-        public static final int BlackPawnFive = 13;
-        public static final int BlackPawnSix = 14;
-        public static final int BlackPawnSeven = 15;
+        public static final int BLACK_KING = 0;
+        public static final int BLACK_QUEEN = 1;
+        public static final int BLACK_KINGS_BISHOP = 2;
+        public static final int BLACK_QUEENS_BISHOP = 3;
+        public static final int BLACK_KINGS_HORSE = 4;
+        public static final int BLACK_QUEENS_HORSE = 5;
+        public static final int BLACK_KINGS_ROOK = 6;
+        public static final int BLACK_QUEENS_ROOK = 7;
+        public static final int BLACK_PAWN_ZERO = 8;
+        public static final int BLACK_PAWN_ONE = 9;
+        public static final int BLACK_PAWN_TWO = 10;
+        public static final int BLACK_PAWN_THREE = 11;
+        public static final int BLACK_PAWN_FOUR = 12;
+        public static final int BLACK_PAWN_FIVE = 13;
+        public static final int BLACK_PAWN_SIX = 14;
+        public static final int BLACK_PAWN_SEVEN = 15;
 
 
-        public static final int WhiteKing = 16;
-        public static final int WhiteQueen = 17;
-        public static final int WhiteKingsBishop = 18;
-        public static final int WhiteQueensBishop = 19;
-        public static final int WhiteKingsHorse = 20;
-        public static final int WhiteQueensHorse = 21;
-        public static final int WhiteKingsRook = 22;
-        public static final int WhiteQueensRook = 23;
-        public static final int WhitePawnZero = 24;
-        public static final int WhitePawnOne = 25;
-        public static final int WhitePawnTwo = 26;
-        public static final int WhitePawnThree = 27;
-        public static final int WhitePawnFour = 28;
-        public static final int WhitePawnFive = 29;
-        public static final int WhitePawnSix = 30;
-        public static final int WhitePawnSeven = 31;
+        public static final int WHITE_KING = 16;
+        public static final int WHITE_QUEEN = 17;
+        public static final int WHITE_KINGS_BISHOP = 18;
+        public static final int WHITE_QUEENS_BISHOP = 19;
+        public static final int WHITE_KINGS_HORSE = 20;
+        public static final int WHITE_QUEENS_HORSE = 21;
+        public static final int WHITE_KINGS_ROOK = 22;
+        public static final int WHITE_QUEENS_ROOK = 23;
+        public static final int WHITE_PAWN_ZERO = 24;
+        public static final int WHITE_PAWN_ONE = 25;
+        public static final int WHITE_PAWN_TWO = 26;
+        public static final int WHITE_PAWN_THREE = 27;
+        public static final int WHITE_PAWN_FOUR = 28;
+        public static final int WHITE_PAWN_FIVE = 29;
+        public static final int WHITE_PAWN_SIX = 30;
+        public static final int WHITE_PAWN_SEVEN = 31;
     }
     
 }
