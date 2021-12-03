b060e855ae9760628b2564acce14d785c469dc68
Mimo604 <56412271+SelimAhmed@users.noreply.github.com>
2021-12-03 13:26:19 -0500

    Update Player.java

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -4,7 +4,7 @@ public class Player {
    private final String NAME;
 
    final ArrayList<Piece> LIVE_PIECES = new ArrayList<Piece>();
-   final ArrayList<Piece> DEAD_PIECES = new ArrayList()<Piece>();
+   final ArrayList<Piece> DEAD_PIECES = new ArrayList<Piece>();
 
    Player(String name) {
       NAME = name;
